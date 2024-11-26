61e87c0edde984343c21c6f0a9e82186ea2daf05
Duykiller04 <haijano04@gmail.com>
2024-11-26 10:39:59 +0700

    Implement job list and filtering functionality

--- routes/company.php
+++ routes/company.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Company\CompaniesController;
 use App\Http\Controllers\Company\HiringsController;
+use App\Http\Controllers\Company\JobsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -38,4 +39,11 @@ Route::group([
     Route::delete('manage-hiring/delete/{id}', [HiringsController::class, 'deleteHiring'])->name('deleteHiring');
     Route::get('search-university', [CompaniesController::class, 'searchUniversity'])->name('searchUniversity');
     
+    Route::get('manage-job', [JobsController::class, 'index'])->name('manageJob');
+    Route::get('manage-job/create', [JobsController::class, 'create'])->name('createJob');
+    Route::post('manage-job/store', [JobsController::class, 'store'])->name('storeJob');
+    Route::get('manage-job/edit/{slug}', [JobsController::class, 'edit'])->name('editJob');
+    Route::put('manage-job/update/{slug}', [JobsController::class, 'update'])->name('updateJob');
+    Route::delete('manage-job/delete/{id}', [JobsController::class, 'destroy'])->name('deleteJob');
+
 });
