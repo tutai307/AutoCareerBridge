f605d0deb3599128e2898689e8fec0f188d1eb9b
TranNhat <nhatcaca2004@gmail.com>
2024-12-20 09:30:21 +0700

    validate user_name

--- app/Http/Controllers/Auth/Management/LoginController.php
+++ app/Http/Controllers/Auth/Management/LoginController.php
@@ -29,12 +29,13 @@ class LoginController extends Controller
     public function checkLogin(LoginRequest $request)
     {
         $data = $request->only('email', 'password');
+        $response = $this->authService->login($data);
 
-        $user = $this->authService->login($data);
-
-        if (empty($user)) {
-            return back()->withInput()->with('error', 'Tài khoản không chính xác.');
+        if(!$response['success']) {
+            return back()->withInput()->with('error', $response['message']);
         }
+
+        $user = $response['user'];
 
         if ($user->role === ROLE_ADMIN || $user->role === ROLE_SUB_ADMIN) {
             return redirect()->route('admin.home')->with('status_success', __('message.login_success'));
