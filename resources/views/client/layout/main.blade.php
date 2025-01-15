797c96e860955c89e46de0940ca5b4ffd7b0471e
Tu Tai <tutai.dev@gmail.com>
2025-01-15 12:37:09 +0700

    first commit

--- resources/views/client/layout/main.blade.php
+++ resources/views/client/layout/main.blade.php
@@ -32,7 +32,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     @yield('css')
     <link rel="shortcut icon" type="image/png" href="{{ asset('clients/images/header/favicon.ico') }}" />
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 
     <script src="{{ asset('toasts/jquery/1.9.1_jquery.min.js') }}"></script>
     <link rel="stylesheet" href="{{ asset('toasts/toast.css') }}">
